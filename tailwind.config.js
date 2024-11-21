cde631bb403be18dd45013f5e4d9fa741b4aa714
Hamza Ateek <hamzaateek@gmail.com>
2024-11-21 09:53:00 +0300

    first tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#F5F7FA",
+        primary: "#4D4D4D",
+        secondary: "#71717",
+        button: "#4CAF4F",
+        title: "#263238",
+      },
+    },
+  },
+  plugins: [],
+};
